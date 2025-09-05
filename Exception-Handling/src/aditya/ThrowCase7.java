f5b88391416ed07168f1bf0234c4256a272908ad
adityaraj3324 <asahu3324@gmail.com>
2025-09-06 02:10:55 +0530

    Throw Case7

--- Exception-Handling/src/aditya/ThrowCase7.java
+++ Exception-Handling/src/aditya/ThrowCase7.java
@@ -1,2 +1,16 @@
-package aditya;public class ThrowCase7 {
+package aditya;
+class A extends RuntimeException{
+    public A(String a){
+        System.out.println(a); //no exception convert manually
+    }
+}
+public class ThrowCase7 {
+    public static void main(String[] args){
+        try {
+            //manually exception create through throw and create a custom exception
+            throw new A("Exception Practice");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }

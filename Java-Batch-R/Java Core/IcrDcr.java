db318a21d4872595d96c47418917a3900a74e4f7
adityaraj3324 <asahu3324@gmail.com>
2025-09-19 23:09:40 +0530

    Increment Decrement

---
+++ Java-Batch-R/Java Core/IcrDcr.java
@@ -0,0 +1,24 @@
+class IcrDcr{
+    public static void main(String[] args) {
+
+        int i = 10;
+
+        System.out.println("Initial value: " + i);
+        System.out.println("Pre-increment (++i): " + ++i); //
+        System.out.println("Value after pre-increment: " + i);
+        i = 10;
+
+        System.out.println("Post-increment (i++): " + i++); //
+        System.out.println("Value after post-increment: " + i);
+
+        i = 10;
+
+        System.out.println("Pre-decrement (--i): " + --i); //
+        System.out.println("Value after pre-decrement: " + i);
+
+        i = 10;
+
+        System.out.println("Post-decrement (i--): " + i--); //
+        System.out.println("Value after post-decrement: " + i);
+    }
+}

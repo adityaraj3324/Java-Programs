5f550bc79c7c009cd33fbcfcbcc6ac51dae85fcc
adityaraj3324 <asahu3324@gmail.com>
2025-09-25 23:25:54 +0530

    Floating-Point Precision

---
+++ Java-Batch-R/Java Core/FloatPrecision.java
@@ -0,0 +1,10 @@
+public class FloatPrecision {
+    public static void main(String[] args) {
+
+        float a = 1.9235897890f;
+        double b = 1.9235897890d;
+        System.out.println("Float value: " + a);
+
+        System.out.println("Double value: "+b);
+    }
+}
